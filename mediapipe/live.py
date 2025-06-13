743b4da04cae3afc4b5f49d4858aada516425ba4
JuanMiguel200 <jrivasagudelo@gmail.com>
2025-06-13 16:16:07 -0500

    mediapipe

--- mediapipe/live.py
+++ mediapipe/live.py
@@ -8,14 +8,14 @@ import threading
 import queue
 
 # ---- SETUP ----
-model_path = 'exported_model/gesture_recognizer.task'
-BaseOptions = mp.tasks.BaseOptions
-GestureRecognizer = mp.tasks.vision.GestureRecognizer
-GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
-VisionRunningMode = mp.tasks.vision.RunningMode
+model_path = '/model/gesture_recognizer.task' #ubicacion del modelo
+BaseOptions = mp.tasks.BaseOptions #importa configuraciones por defecto
+GestureRecognizer = mp.tasks.vision.GestureRecognizer #tipo de tarea
+GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions #objeto que almacena las opciones
+VisionRunningMode = mp.tasks.vision.RunningMode #modo de obtención de los frames (imagen, video, en vivo)
 
 result_queue = queue.Queue()
-
+ # marca los 21 puntos clave de la mano y escribe el gesto reconocido recibe en result un array con las coordenadas y el gesto detectado y en output_image el frame en el que esto ocurrió
 def gesture_recognizer_callback(result, output_image, timestamp_ms):
     annotated_frame = np.copy(output_image.numpy_view())
 
@@ -33,9 +33,6 @@ def gesture_recognizer_callback(result, output_image, timestamp_ms):
         text = f'{gesture_name} ({score:.2f})'
         cv2.putText(annotated_frame, text, (30, 50),
                     cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 0, 0), 3)
-        if gesture_name == 'rock':
-            cv2.putText(annotated_frame, "SAPA", (50, 80),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3)
 
     # Enviar imagen anotada al hilo principal
     result_queue.put(annotated_frame)
@@ -45,19 +42,19 @@ options = GestureRecognizerOptions(
     base_options=BaseOptions(model_asset_path=model_path),
     running_mode=VisionRunningMode.LIVE_STREAM,
     result_callback=gesture_recognizer_callback
-)
+) #se definen las configuraciones base del reconocimiento
 
 with GestureRecognizer.create_from_options(options) as recognizer:
     cap = cv2.VideoCapture(0)
     timestamp = 0
-
+    #Llama a opencv para capturar las imagenes
     while cap.isOpened():
         success, frame = cap.read()
         if not success:
             print("No se pudo leer la cámara.")
             break
 
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)#convierte la imagen de bgr a rgb para ser procesada por mediapipe
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
 
         # Enviar imagen
@@ -75,3 +72,4 @@ with GestureRecognizer.create_from_options(options) as recognizer:
 
     cap.release()
     cv2.destroyAllWindows()
+
